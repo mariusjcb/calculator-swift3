5e22bd48bcd9a174cb89a209821e27749de0cd70
Marius Ilie <marius_ilie20082001@yahoo.com>
2016-12-23 13:17:11 +0200

    193P Stanford University - L3 Calculator v2

---
+++ Calculator/CalculatorBrain.swift
@@ -0,0 +1,102 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Marius Ilie on 22/12/16.
+//  Copyright © 2016 University of Bucharest - Marius Ilie. All rights reserved.
+//
+
+import Foundation
+
+final class CalculatorBrain {
+    private var accumulator = 0.0
+    private var internalProgramLog = [AnyObject]()
+    
+    private enum Operation {
+        case Constant(Double)
+        case UnaryOperation((Double) -> Double)
+        case BinaryOperation((Double, Double) -> Double)
+        case Equals
+    }
+    
+    private var operations: Dictionary<String, Operation> = [
+        "π" : Operation.Constant(M_PI),
+        "e" : Operation.Constant(M_E),
+        "√" : Operation.UnaryOperation(sqrt),
+        "cos": Operation.UnaryOperation(cos),
+        "×" : Operation.BinaryOperation({ $0 * $1  }),
+        "÷" : Operation.BinaryOperation({ $0 / $1  }),
+        "+" : Operation.BinaryOperation({ $0 + $1  }),
+        "−" : Operation.BinaryOperation({ $0 - $1  }),
+        "=" : Operation.Equals
+    ]
+    
+    private var pending: PendingBinaryOperationInfo?
+    
+    private struct PendingBinaryOperationInfo {
+        var binaryFunction: (Double, Double) -> Double
+        var firstOperand: Double
+    }
+    
+    private func executePandingBinaryOperation() {
+        if pending != nil {
+            accumulator = pending!.binaryFunction(pending!.firstOperand, accumulator)
+            pending = nil;
+        }
+    }
+    
+    func setOperand(_ operand: Double) {
+        self.accumulator = operand
+        internalProgramLog.append(operand as AnyObject);
+    }
+    
+    func performOperation(_ symbol: String) {
+        if let operation = operations[symbol]
+        {
+            switch operation {
+            case .Constant(let associatedConstantValue):
+                accumulator = associatedConstantValue
+            case .UnaryOperation(let closure):
+                accumulator = closure(accumulator)
+            case .BinaryOperation(let closure):
+                executePandingBinaryOperation()
+                pending = PendingBinaryOperationInfo(binaryFunction: closure, firstOperand: accumulator)
+            case .Equals:
+                executePandingBinaryOperation()
+            }
+        }
+        internalProgramLog.append(symbol as AnyObject);
+    }
+    
+    typealias PropertyList = AnyObject
+    
+    var programLog: PropertyList {
+        get {
+            return internalProgramLog as CalculatorBrain.PropertyList
+        }
+        set {
+            clear()
+            if let arrayOfOps = newValue as? [AnyObject] {
+                for op in arrayOfOps {
+                    if let operand = op as? Double {
+                        setOperand(operand)
+                    } else if let operation = op as? String {
+                        performOperation(operation)
+                    }
+                }
+            }
+        }
+    }
+    
+    var result: Double {
+        get {
+            return accumulator
+        }
+    }
+    
+    func clear() {
+        accumulator = 0.0
+        pending = nil
+        internalProgramLog.removeAll()
+    }
+}
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -6,20 +6,69 @@
 //  Copyright © 2016 University of Bucharest - Marius Ilie. All rights reserved.
 //
 
+// filmora
+
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var display: UILabel!
+    var userIsInTheMiddleOfTyping = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func onTouchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if(userIsInTheMiddleOfTyping)
+        {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
+    }
+    
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    lazy var brain = CalculatorBrain();
+    @IBAction func performOperation(_ sender: UIButton) {
+        if(userIsInTheMiddleOfTyping) {
+            brain.setOperand(displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        if let mathematicalSymbol = sender.currentTitle {
+            brain.performOperation(mathematicalSymbol)
+        }
+        displayValue = brain.result
+    }
+    
+    var savedProgram: CalculatorBrain.PropertyList?
+    @IBAction func save() {
+        savedProgram = brain.programLog
+    }
+    
+    @IBAction func restore() {
+        if savedProgram != nil {
+            brain.programLog = savedProgram!
+            displayValue = brain.result
+        }
+    }
+    
 }
 
